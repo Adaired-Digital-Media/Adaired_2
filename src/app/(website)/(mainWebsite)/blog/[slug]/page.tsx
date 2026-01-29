bfbd7563081297488118f77dc496777a12b3a7d1
anujaraired <anuj@adaired.org>
2026-01-29 10:48:38 +0530

    test git

--- src/app/(website)/(mainWebsite)/blog/[slug]/page.tsx
+++ src/app/(website)/(mainWebsite)/blog/[slug]/page.tsx
@@ -92,7 +92,7 @@ const Blog = async ({ params }: BlogProps) => {
   const h2Total = (bodyHtml.match(/<h2>/g) || []).length;
   let currentH2Index = 0;
   let isLastH2Section = false;
-//test git config
+//test git config 12121
   return (
     <>
       <PageBanner title={blogData.bannerTitle} />
