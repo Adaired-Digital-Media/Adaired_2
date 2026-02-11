3caedf6e55bc1f19292e0014af27585fef93f946
anujaraired <anuj@adaired.org>
2026-02-11 18:59:35 +0530

    fd

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -17,7 +17,7 @@ export default function DefaulWebsitetLayout({
           <ReactLenis root>{children}</ReactLenis>
         </WebsiteLayout>
         <div className="fixed bottom-2 left-3 cursor-pointer">
-          <Link href="https://wa.me/8907300008" target="_blank">
+          <Link href="https://wa.me/918907300008" target="_blank">
             <Image
               src={whatsApp}
               width={55}
