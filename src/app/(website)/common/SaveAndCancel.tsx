25c163aac7928054969858bdfe92183f66fbcc8c
anujaraired <anuj@adaired.org>
2026-02-16 18:34:40 +0530

    created web and local seo page

--- src/app/(website)/common/SaveAndCancel.tsx
+++ src/app/(website)/common/SaveAndCancel.tsx
@@ -43,7 +43,7 @@ const SaveAndCancel = ({
         onMouseEnter={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)}
         onClick={handleClick}
-        className={`px-[2rem] ${isBorder ? 'border-[1px] border-black' : 'border-[1px] border-transparent'} ${isHover && 'border-[1px] border-transparent'} flex w-full cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-[60px] py-[0.75rem] font-montserrat font-medium transition-all duration-300 ease-out active:scale-95 ${
+        className={`px-[2rem] ${isBorder ? 'border-[1px] border-black' : 'border-[1px] border-transparent'} ${isHover && 'border-[1px] border-transparent'} flex w-full cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-[60px] py-[0.75rem] font-montserrat text-xxs font-medium transition-all duration-300 ease-out active:scale-95 xl:text-xs 1366:text-[14px] 1400:text-[18px] 1470:text-[18px] 1600:text-[18px] 1680:text-[18px] ${
           isBgWhite
             ? isHover
               ? isHoverBgBlue
@@ -66,7 +66,7 @@ const SaveAndCancel = ({
       {is2ndButton && (
         <button
           onClick={handleClick2}
-          className={`group flex w-full cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-[60px] px-[2rem] py-[0.75rem] font-montserrat font-medium transition-all duration-300 ease-out active:scale-95 ${
+          className={`${widthClass} group flex w-full cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-[60px] py-[0.75rem] font-montserrat text-xxs font-medium transition-all duration-300 ease-out active:scale-95 xl:text-xs 1366:text-[14px] 1400:text-[18px] 1440:text-[18px] 1470:text-[18px] 1600:text-[18px] 1680:text-[18px] ${buttonWidth} ${
             is2BgWhite
               ? 'border border-[#111111] bg-white text-[#111111] hover:bg-[#1A5A96] hover:text-white'
               : 'bg-[#FB9100] text-white hover:bg-[#1A5A96]'
